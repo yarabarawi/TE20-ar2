e694a583051f24efdaf1e3b07ac3fb7fe8cbc4bb
yara mustafa <mustafa.yara@elev.ga.ntig.se>
2021-09-22 11:08:15 +0200

    vi har lärt oss if-satser och to lower()

---
+++ kapitel 3/interaktiv berättelse/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace interaktiv_berättelse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Det finns två kistor tar du kista ett eller kista två");
+            string kista = Console.ReadLine();
+
+
+            if (kista == "ett")
+            {
+                Console.WriteLine("Du valde kista ett, du fick ett blått piller");
+            }
+
+            if (kista == "två")
+            {
+                Console.WriteLine("Du valde kista två du fick ett rött piller");
+            }
+
+            Console.WriteLine("Du ser en knapp på väggen, trycker du på den eller inte");
+
+            if (knapp == "tryck")
+            {
+                System.Console.WriteLine("Nu hör du en röst viska, ät pillret eller gå upp för trappan");
+            }
+
+        }
+    }
+}
---
+++ kapitel 3/vilkor/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace vilkor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hur gammal är du? (heltal)");
+            int ålder = int.Parse(Console.ReadLine());
+
+            // "om ålder större än 18 du får ta körkort"
+            if (ålder >= 18)
+            {
+                Console.WriteLine( "Du får ta körkort"  );
+            }
+            
+            // om ålder är 15 eller högre du får ta moped körkort
+
+            if (ålder >= 15)
+            {
+                Console.WriteLine("Du får ta moped körkort");
+            }
+      
+            // Fråga användaren "Vad heter Abbas senaste Albumet?"
+            Console.WriteLine("Vad heter Abbas senaste Albumet?");
+            string låt = Console.ReadLine();
+
+
+
+            if (låt == "Voyage" || låt == "voyage")
+            {
+                Console.WriteLine("Bra svarat");
+            }
+            
+            else
+            {
+                Console.WriteLine("Fel! Albumet heter Voyage");
+            }
+            
+            // sista TP-fråga
+            Console.Write("Vem missade straffen för Frankrike");
+
+            // läs in och tvinga till små bokstäver:
+            // Mbappe -> mbappe
+            // mBappe -> mbappe
+            string spelare = Console.ReadLine();
+            
+            if (spelare == "mbappe")
+            {
+                Console.WriteLine("Bra, du är en expert!");
+            }
+
+            else
+            {
+                Console.WriteLine("Fel! Det var Mbappe");
+            }
+
+        }
+    }
+}
