629db3371c3d1325ba273c5b3e92b89ff7a85e3d
yara mustafa <mustafa.yara@elev.ga.ntig.se>
2021-09-28 10:30:18 +0200

    flowchart

---
+++ kapitel 3/engflowchart/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+
+namespace engflowchart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.Clear();
+            Console.WriteLine("Engineering Flowchart");
+
+            Console.Write("Does it move? (y/n)");
+            string answer1 = Console.ReadLine();
+            if (answer1 == "y")
+            {
+                Console.Write("should it? (y/n)");
+                string answer2 = Console.ReadLine();
+                if (answer2 == "y")
+                {
+                    Console.WriteLine("No problem! 😁");
+                }
+                else
+                {
+                    Console.WriteLine("Use Duct tape!");
+                }
+            }
+            else
+            {
+                Console.Write("Should it? (y/n)");
+                string answer3 = Console.ReadLine();
+                if (answer3 == "n")
+                {
+                    Console.WriteLine("No problem!😁");
+                }
+                else
+                {
+                    Console.WriteLine(" Use WD-40!");
+                }
+            }
+        }
+    }
+}
